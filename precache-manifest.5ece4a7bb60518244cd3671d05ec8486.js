e299c0176acb9b9780a74aea1bf1bfbe86a82d2a
Vyacheslav Hludnev <vj.hludnev@gmail.com>
2021-06-06 11:04:22 +0300

    Updates

---
+++ precache-manifest.5ece4a7bb60518244cd3671d05ec8486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8979929d8898a73786ac639ffd85ba0d",
+    "url": "/todo-list-app2/index.html"
+  },
+  {
+    "revision": "708028e782e97d113b7c",
+    "url": "/todo-list-app2/static/css/main.c9b3a235.chunk.css"
+  },
+  {
+    "revision": "522a8d7a69960465ff6d",
+    "url": "/todo-list-app2/static/js/2.2774d78f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/todo-list-app2/static/js/2.2774d78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708028e782e97d113b7c",
+    "url": "/todo-list-app2/static/js/main.9266eb5a.chunk.js"
+  },
+  {
+    "revision": "5441e6d1e6ed6e6eb83a",
+    "url": "/todo-list-app2/static/js/runtime-main.62b41a97.js"
+  }
+]);
